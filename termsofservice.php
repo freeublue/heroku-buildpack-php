818ed48e87b2aea48fedba73f94d6f4776df0f12
Susan Hern <freeublue@gmail.com>
2018-07-14 19:02:08 +0200

    Add files via upload

---
+++ termsofservice.php
@@ -0,0 +1,113 @@
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+<style>
+@import "https://fonts.googleapis.com/css?family=Raleway:100,300,600";
+
+
+body, input, select, textarea {
+		color: #9a9a9a;
+		font-family: "Raleway", Arial, Helvetica, sans-serif;
+		font-size: 13pt;
+		font-weight: 300;
+		line-height: 1.65;} 
+th{
+padding:5px;
+font-family: Arial Rounded MT Bold,Helvetica Rounded,Arial,sans-serif; 
+font-size:20px;
+color:white;
+Background: #5385c1;
+Border: 1px solid #5385c1;
+ text-align: center;
+border-top-left-radius: 2em 0.5em;
+border-top-right-radius: 2em 0.5em;
+border-bottom-right-radius: 0em;
+border-bottom-left-radius: 0em;}
+tr{border-radius:15%;}
+td{background: white;
+ text-align: center;
+color:#5385c1;;
+border-bottom:1px dotted #5385c1;
+border-left: 1px solid #5385c1;
+padding:4px;
+font-family: Verdana,Geneva,sans-serif; 
+
+}
+table{border-radius:15%;
+margin-bottom:25px;
+width:400px;
+border-bottom: 2px solid #5385c1;}
+#nav{width:100%;
+height:50px;
+
+background:white;}
+a{text-decoration:none;}
+.logo{height:30px;
+float:right;
+margin:2px;
+width:100px;
+padding:10px;
+background:#5385c1;
+border-radius:15%;
+color:white;}
+.buttonsmall{
+height:30px;
+width:250px;
+padding:10px;
+background: #5385c1;
+color:white;}
+h1{color:white;
+margin-left:500px;
+margin-top:400px;
+font-family: 'Lobster', cursive;}
+.header1{
+font-family: "Raleway", Arial, Helvetica, sans-serif;
+		font-size: 20pt;
+		font-weight: 400;
+text-align:center;
+color: #5385c1;}
+.header{
+font-family: "Raleway", Arial, Helvetica, sans-serif;
+		font-size: 20pt;
+		font-weight: 400;
+color: #5385c1;}
+.header2{
+font-family: "Raleway", Arial, Helvetica, sans-serif;
+		font-size: 18pt;
+		font-weight: 250;
+color: white;}
+.ppfull{clear:left;
+width:100%;
+border-bottom:1px silver dotted;
+text-align:left;
+padding:5px;
+margin:40px;}
+.pp{float:left;
+width:30%;
+border-right:1px silver dotted;
+text-align:left;
+padding:5px;
+margin:4px;}
+.blockblue{float:left;
+width:25%;
+height:400px;
+border-right:1px white solid;
+text-align:left;
+padding:5px;
+margin:20px;
+background:#5385c1;
+}
+.imageround{
+border-radius:50%;
+padding:10px;
+border:white dotted 2px;
+}
+.image12{
+width:100%;
+}
+p{color:#5a5a5a;
+font-size:18px;
+font-family: "Raleway", Arial, Helvetica, sans-serif;}</style>
+<h2>Terms of Service</h2>
+<p>To become a contractor you must be in possession of a smartphone to run the applications</p>
+<p>By applying to become a Laundry By Magic partner, you understand that you are not appling for a job, but to become an independent contractor, a business owner who recieves work from Laundry By Magic whose capacity under this agreement is to act as an agency, handing out laundry to be collected, cleaned and delivered, from/to clients solicited by Laundry By Magic.</p>
+<p>As a contractor offering to drive as a partner to Laundry By Magic, you are required to complete the application form online(on our website or using our mobile application). If selected you must log on to our application to recieve delivery contracts. You may select any area to work in, as you will have work allocted accoring to you Latitude and Longitude co-ordinates displayed to customers by the mobile application. If logged in, you will be selected as and when contracts become available for fullfilment within a 30km radius of you position, meaning that no delivery or collection will be at a greater distance. Your services will be paid for on completion of drop-off(at laundry contractor) and again on completion of delivery(back to client). Each trip earns R40.5, you may run several trips combined if work is available, without reduction of delivery fee. One client per hour earns a minimum of R81, while running 5 clients simultaneously will earn R405 per hour. As an independent contractor you are responsible for your petrol and vehicle maintenace costs.</p>
+<p>As a contractor offering to launder as a partner to Laundry By Magic, you are required to complete the application form online(on our website or using our mobile application). If selected you must log on to our application to recieve delivery contracts. You will be allocated work within a radius of 30km around your address, which will be displayed to potential clients by the mobile application and/or online application/website. Fees paid to laundry contractors are clearly displayed on our website(You receive the listed price by service, less commision paid to Laundry By Magic for their solicting), these fees my fluctuate peroidically.(You will be advised in advance of any changes.)</p><p>Earning potential is totally dependent on quantity of clients in your area of operation. The minimum fee is R28.8 per half hour(machine wash, no fold) this is for one load(6kg) of washing which should take 20min. Wash and fold R37.44 per load(6kg) of laundry which should take 20min. Iron, wash, fold per load(6kg) which should take 30min, R79.20. Thus to wash and fold only, per hour, with only one machine you should be able to earn R112.44 per hour.</p><p>You are responsible to register your business with TAX authorities. in South Africa the best method for your small business is "Turn Over Tax" which has been specially designed for small business owners, you only pay if your earnings exceed a set amount.</p>
