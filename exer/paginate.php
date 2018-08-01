3ebaea41775249a56f878d3a417babd531c8ef0a
Susan Hern <freeublue@gmail.com>
2018-08-01 18:55:08 +0200

    Add files via upload

--- exer/paginate.php
+++ exer/paginate.php
@@ -19,7 +19,7 @@ $num = $rec*10;
 echo "<a href='" . $_SERVER['PHP_SELF'] . "?nu=" . ($num) . "'>$p </a>";
 }
 echo "<br />";
-if(($nu+10) >= $rescount) { 
+if(($nu+10) >= $rescount && $pages != 0) { 
 
 echo "<a href='" . $_SERVER['PHP_SELF'] . "?nu=" . ($nu-10) .  "'>Previous Page </a><br />"; } 
 elseif ($nu+10 < $rescount && $nu > 1) { 
