f176d32a58c8d04df5f697b651fb6c5560ac440e
Susan Hern <freeublue@gmail.com>
2018-08-01 12:15:21 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,185 +1,190 @@
 <?
-session_start();
-include "generategeo.php";
-$foo = $_SERVER[SERVER_NAME];
-
-
-if($foo === 'laundrybymagic.herokuapp.com') { 
+require "style.php";
 ?>
-<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-<style>
-@import "https://fonts.googleapis.com/css?family=Raleway:100,300,600";
-td{width:50%;}
-a{text-decoration:none;
-color:#5385c1;}
-.kk{
-  margin: 0;
-  padding: 0;
-}
-.kk li {
-  margin: 0;
-  padding-bottom: 12px;
-padding-left: 10px;
-  list-style: none;
-color: navy;
-  background: url('checkmark2.jpg') no-repeat 0 50%;
+
+<!DOCTYPE HTML><html><head>
+<title>User Project</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+<a class='logo' href="index.php">Add User</a>
+<a class='logo' href="tables.php">Select JS</a>
+<a class='logo' href="selectphp.php">Select PHP</a>
+<a class='logo' href="byname.php">By name</a>
+<a class='logo' href="byage.php">By Age</a>
+<a class='logo' href="bymobile.php">By mobile</a>
+<a class='logo' href="byemail.php">By email</a><br /><hr>
+<script>
+function validateForm()
+{
+var x=document.forms["rform"]["name"].value;
+if (x==null || x=="")
+{
+alert("Name must be filled out");
+return false;
 } 
 
-@import "https://fonts.googleapis.com/css?family=Raleway:100,300,600";
 
+var xc=document.forms["rform"]["surname"].value;
+if (xc==null || xc=="")
+{
+alert("Surname must be supplied");
+return false;
+} 
+
+var xc2=document.forms["rform"]["mobile"].value;
+if (xc2==null || xc2=="")
+{
+alert("Mobile number must be supplied");
+return false;
+} 
+
+
+
+var xc22=document.forms["rform"]["email"].value;
+if (xc22==null || xc22=="")
+{
+alert("Email Address must be supplied");
+return false;
+} 
+var xc3=document.forms["rform"]["birthdate"].value;
+if (xc3==null || xc3=="")
+{
+alert("Day of Birth must be supplied");
+return false;
+} 
+
+} 
+</script><style>
 
 body, input, select, textarea {
 		color: #9a9a9a;
 		font-family: "Raleway", Arial, Helvetica, sans-serif;
-		font-size: 18pt;
+		font-size: 13pt;
 		font-weight: 300;
 		line-height: 1.65;} 
+input, textarea { 
+    padding: 9px; 
+    border: solid 1px #E5E5E5; 
+    outline: 0; 
+    font: normal 13px/100% Verdana, Tahoma, sans-serif; 
+    width: 200px; 
+    background: #FFFFFF left top repeat-x; 
+    background: -webkit-gradient(linear, left top, left 25, from(#FFFFFF), color-stop(4%, #EEEEEE), to(#FFFFFF)); 
+    background: -moz-linear-gradient(top, #FFFFFF, #EEEEEE 1px, #FFFFFF 25px); 
+    box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+    -moz-box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+    -webkit-box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+    } 
+   
+textarea { 
+    width: 400px; 
+    max-width: 400px; 
+    height: 150px; 
+    line-height: 150%; 
+    } 
+   
+input:hover, textarea:hover, 
+input:focus, textarea:focus { 
+    border-color: #C9C9C9; 
+    -webkit-box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 8px; 
+    } 
+   
+label { 
+    margin-left: 10px; 
+    color: #999999; 
+    } 
 
-#nav{width:100%;
-background-repeat:no-repeat;
-height:600px;
-background-image: url("banner.jpg");}
-
-.logo{height:30px;
-float:right;
-margin:2px;
-width:100px;
-padding:10px;
-background:#5385c1;
+form{background:blue;
+padding:30px;
+width:400px;
+border:gray dotted 1px;
 border-radius:15%;
-color:white;}
-.buttonsmall{
-height:30px;
-width:250px;
-padding:10px;
-font-size: 18pt;
-background: #5385c1;
-color:white;}
-h1{color:white;
-margin-left:400px;
-margin-top:400px;
-font-family: 'Lobster', cursive;}
-.header{
-font-family: "Raleway", Arial, Helvetica, sans-serif;
-		font-size: 20pt;
-		font-weight: 400;
-color: #5385c1;}
-.ppc{float:right;
-width:40%;
-border-bottom:1px silver dotted;
-text-align:left;
-padding:5px;
-margin:50px 4px 4px 4px;}
-.pp5{float:right;
-width:40%;
-border-bottom:1px silver dotted;
-border-right:1px silver dotted;
-text-align:left;
-padding:5px;
-margin:50px 4px 4px 4px;}
-.pp4{clear:left;
-width:100%;
-border-bottom:1px silver dotted;
-border-right:1px silver dotted;
-text-align:left;
-padding:5px;
-margin:50px 4px 4px 4px;}
-.ppl{float:left;
-width:45%;
-border-bottom:1px silver dotted;
-text-align:left;
-padding:5px;
-margin:50px 4px 4px 4px;}
-.pp{float:left;
-height:500px;
-width:30%;
-border-right:1px silver dotted;
-text-align:left;
-padding:5px;
-margin:4px;}
-p{color:#5a5a5a;
-font-size:22px;
-font-family: "Raleway", Arial, Helvetica, sans-serif;}
-.medimg1{border:#5385c1 25px solid;
-border-radius:40%;
-float:right;}
-.medimg2{border:#5385c1 25px solid;
-border-radius:40%;
-float:left;}
-.hh{
-color:#5a5a5a;
-font-style:italic;
-font-weight:400;
-font-size:18px;
-font-family: "Raleway", Arial, Helvetica, sans-serif;
-clear:left;
-padding:4px;
-width:300px;} 
-.subs_popup {
-padding:20px;
-color:white;
-	display: block;
-	background-color: rgba(0,0,0,0.65);
-	position: absolute;
-top: 150px;
-left:100px;
-	height: 500px;
-	width: 600px;
-z-index: 5;
-} 
-#results{display:none;
-background:gray;} 
-#close_popup{float:right;}
+font-size:20px;
+background: -webkit-gradient(linear, left top, left 25, from(#FFFFFF), color-stop(4%, #EEEEEE), to(#FFFFFF)); 
+background: -moz-linear-gradient(top, #FFFFFF, #EEEEEE 1px, #FFFFFF 25px); 
+box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+    -moz-box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+    -webkit-box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+}
 </style>
+
 <?php
-$tin = "Door to Door Laundry Service Laundry By Magic";
-$ke = "laundry on demand, online laundry, laundry service, wash clothes, door to door laundry, get laundry, laundry, wash, iron, dry clothes, clean clothes, clean curtains, clean linen, launder linen, wash bedding, laundry service";
-$de = "laundry service to wash and iron";
-include "meta.php";
-include "slide.php";
-?>
-<body>
-<div id='nav'><a class='logo' href="index.php">Home</a>
+include 'lib.php';
+$name = "name";
+$surname = "surname";
+$mobile= "mobile";
+$email = "email";
+$birthdate = "birthdate";
 
-<a class='logo' href="register.php">Register</a>
-<a class='logo' href="login.php">Login</a>
-<a class='logo' href="prices.php">Prices</a>
-<a class='logo' href="require.php">Require</a>
-<a class='logo' href="offer.php">Offer</a><br /><h1>Laundry By Magic</h1></div>
-<table cellpadding='6' width='auto'><tr><td class='pp' width='30%'valign='top'><div class='header'>BOOK A WASH.<br />Instant online calculator. No hidden price shocks. Pay online.</div><p>Our drivers collect from your door, organise cleaning then return the laundered items back to your door.</p><br /><a href="map/geo5.php" class="buttonsmall">Book</a></td>
+if(!isset($_POST[submit])) { 
+echo "<h1>User.</h2>";
+echo "<form action='$_SERVER[PHP_SELF]' name='rform' onsubmit='return validateForm();' method='post'>";
+echo "<h2>Complete Your Profile.</h2>";
+echo "<b>First Name</b></br >";
+text($name);
+echo "<br /><b>Surname</b></br >";
+text($surname);
 
-<td class='pp' valign='top'><div class='header'>GET A QUOTE.<br />Unsure about pricing? No problem get a quote instantly.</div><p>Before you make a booking let our online calculator offer you quick and easy quotes.</p><br /><a href="quote.php" class="buttonsmall">Quote</a></td>
+echo "<br /><b>Cellphone Number</b></br >";
+text($mobile);
 
-<td class='pp' valign='top'><div class='header'>FAIR PRICES.<br />Equitable, transparent, pricing for peace of mind.</div><p>Transparent pricing based on weight. Never pay more per kg than quoted.</p><br /><a href="prices.php" class="buttonsmall">Prices</a></td></tr></table><table>
+echo "<br /><b>Email Address</b></br >";
+text($email);
+echo "<br /><b>Date of Birth </b></br>";
+echo "<input type='date' name='$birthdate' />";
 
-<tr><td class='pp1' valign='top'><div class='header'>Empower people with your choice.</div></center><img class='medimg2' width='300px' src='images/pic01.jpg' /><center><div class='hh'>By entrusting us with your laundry you are empowering and employing people who might otherwise have very limited income. You are assising small business and helping the economy GROW.</div></td><td></td>
-<td class='pp1' valign='top'><div class='header'>Focus on Life not Laundry</div><img class='medimg1' width='300px' src='images/pic02.jpg' /><div class='hh'>Allow us to sort your laundry! Our low prices mean that you can focus on things that matter to you and forget about the laundry. We deliver door-to-door with a 24hr turnaround timeframe.</div></td></tr></table>
 
-<div class='pp4'><table width='100%'><tr><td>
-
-<a href='addcontractor.php'>Apply to become a Contractor</a><br />
-<a href='termsofservice.php'> TandC </a></td><td>
-
-<form action='nlet.php' method='post'><b>Name</b><br /><input type='text' name='name' /><br /><b>Email</b><br /><input type='text' name='ema' /><br /><input type='submit' name='submit' value='GET IT!' /></form><br /></td></tr></table></div>
-<div class='subs_popup'><h2>Get our newsletter for special offers in your inbox</h2><div onclick='close_pu();' id='close_popup'>X</div><img src='mail.jpg' style='float:right;' height='150px'/>
-<form name='mailform'><h4>Subscribe</h4><b>Name: </b><input style='color:gray' type='text' name='nam' /><br /><b>Email: </b><input type='text' style='color:gray' name='mail' /><br /><center><div class="btn btn-default" onclick='showmail();' />SUBSCRIBE</div></center></form><div id='results'>x</div></div>
-
-</body>
-<? } else { 
-echo " "; } 
-?>
-<script src="jquery-1.9.1.js"></script>
-<script>
-var mail;
-var nam;
-function showmail() { 
-$.ajaxSetup({ cache: false });
-$("#results").show();
-var nam = document.mailform.nam.value;
-var mail = document.mailform.mail.value;
-var url = "addmail.php?nam="+nam+"&&mail="+mail;
-$('#results').load(url);
+foreach(range(1, 16) as $f){ 
+echo "<option value='$f'>$f</option>";
 } 
-function close_pu() { 
-$(".subs_popup").hide();
-} </script>
+echo "</select><br />";
+include "Robochkr.php";
+echo "<br /><input type='submit' style='background:black;height:50px;color:white;font-size:25px;' name='submit' value='submit' />";
+echo "</form>";
+} else {
+
+if(EMPTY($_POST["chkhuman"]) ) { 
+echo "please add the Robot checker sum";
+} 
+elseif($_POST["chk"] != $_POST["chkhuman"]) { 
+echo "That is incorrect, please try again"; } 
+elseif(EMPTY($_POST["name"]) ) { 
+echo "Name must be supplied"; } 
+elseif(EMPTY($_POST["surname"]) ) { 
+echo "Surname must be supplied"; } 
+elseif(EMPTY($_POST["birthdate"]) ) { 
+echo "birthdate must be supplied"; } 
+elseif(EMPTY($_POST["email"]) ) { 
+echo "Email must be supplied"; } 
+elseif((filter_var($_POST[email], FILTER_VALIDATE_EMAIL) != true)) { 
+echo "Email is in the incorrect format"; } 
+
+elseif(EMPTY($_POST["mobile"]) ) { 
+echo "Mobile number supplied"; } 
+elseif(strlen($_POST['mobile']) != 10) {
+echo "mobile number is not 10 numbers long"; }
+ elseif (preg_replace("/[^0-9]/", "", $_POST['mobile']) != $_POST['mobile']) {
+            echo "Mobile may be numbers only";
+        } 
+
+else { 
+ 
+$afname = strip_tags(htmlentities($_POST["name"]));
+$alname = strip_tags(htmlentities($_POST["surname"]));
+$amobileno = strip_tags(htmlentities($_POST["mobile"]));
+$bdate = trim($_POST["birthdate"]);
+$emm = trim($_POST["email"]);
+$ema = filter_var($emm, FILTER_SANITIZE_EMAIL);
+$day = date("Y-m-d H:i:s");
+require "con.php";
+$sql = "INSERT INTO users (name, surname, mobile, email, birthdate, ins_time) VALUES (?,?,?,?,?,?)";
+$pdo->prepare($sql)->execute([$afname, $alname, $amobileno, $ema, $bdate, $day]);
+echo "<a href='index.php'>Record added go back and add another</a>";
+ } } 
+
+?>
+
+
+
---
+++ tables.php
@@ -0,0 +1,105 @@
+<?
+require "style.php";
+?>
+<!DOCTYPE html>
+<html lang="en"><head><link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet"><link href="bootstrap.min.css" rel="stylesheet"><link href="font-awesome.min.css" rel="stylesheet" type="text/css"><script src="https://code.jquery.com/jquery-3.3.1.js"></script><script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script><meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>User Tables</title>
+  <!-- Bootstrap core CSS-->
+  <link href="bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Page level plugin CSS-->
+  <link href="dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="sb-admin.css" rel="stylesheet">
+</head>
+
+<body><a class='logo' href="index.php">Add User</a>
+<a class='logo' href="tables.php">SelectJS</a>
+<a class='logo' href="byname.php">SelectJS</a>
+<a class='logo' href="selectphp.php">SelectPHP</a><br /><hr>
+
+ 
+      
+
+      <!-- Example DataTables Card-->
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i>Users</div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Mobile</th>
+                  <th>Date of Birth</th>
+                  <th>Age</th>
+                  <th>Start Date</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th>Name</th>
+                  <th>Mobile</th>
+                  <th>Date of Birth</th>
+                  <th>Age</th>
+                  <th>Start Date</th>
+                  <th>Email</th>
+                </tr>
+              </tfoot>
+              <tbody>
+              <?php
+              require "con.php";
+              
+       $stmt = $pdo->query("SELECT * FROM users");
+while ($row = $stmt->fetch()) {
+    
+
+
+
+            echo "<tr>";
+            
+               echo "<td>" . $row["name"]. ' ' . $row["surname"] . "</td>";
+               
+               echo "<td>" . $row["mobile"] . "</td>"; 
+               echo "<td>" . $row["birthdate"] . "</td><td>ggg" . $ageuser . "</td><td>" . $row["ins_time"] . "</td><td>" . $row["email"] . "</td>"; 
+               echo "</tr>";
+               
+
+               } 
+               ?>
+              
+             
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+      </div>
+    </div>
+    <!-- /.container-fluid-->
+    <!-- /.content-wrapper-->
+    <footer class="sticky-footer">
+      <div class="container">
+        <div class="text-center">
+          <small>Copyright © Project 2018</small>
+        </div>
+      </div>
+    </footer>
+    
+<script>
+$(document).ready(function() {
+    $('#dataTable').DataTable();
+} );
+</script>
+    
+  </div>
+</body>
+
+</html>
