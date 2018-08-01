1926adc3310947f5cb015f3168307c60035f1352
Susan Hern <freeublue@gmail.com>
2018-08-01 12:27:10 +0200

    Add files via upload

---
+++ exer/paginate.php
@@ -0,0 +1,35 @@
+
+<?php
+$rescount = $records;
+$recordsperpage = 10;
+$pages = ceil($rescount/10);
+echo "Pages $pages<br />";
+
+if(!($_GET["nu"])) {  
+ $nu = 0;} 
+else {  $nu = $_GET[nu]; } 
+$ref = ($nu +1);
+$pg = range(1, $pages, 1);
+
+
+foreach( $pg as $p) { 
+$rec = $p - 1;
+$num = $rec*10;
+
+echo "<a href='" . $_SERVER['PHP_SELF'] . "?nu=" . ($num) . "'>$p </a>";
+}
+echo "<br />";
+if(($nu+10) >= $rescount) { 
+
+echo "<a href='" . $_SERVER['PHP_SELF'] . "?nu=" . ($nu-10) .  "'>Previous Page </a><br />"; } 
+elseif ($nu+10 < $rescount && $nu > 1) { 
+
+echo "<a href='" . $_SERVER['PHP_SELF'] . "?nu=" . ($nu+10) . "'>Next Page </a><br />";
+echo "<a href='" . $_SERVER['PHP_SELF'] . "?nu=" . ($nu-10) .  "'>Previous Page </a><br />"; } elseif($nu === 0) { 
+
+echo "<a href='" . $_SERVER['PHP_SELF'] . "?nu=" . ($nu+10) .  "'>Next Page </a><br />";
+} 
+
+
+
+?> 
