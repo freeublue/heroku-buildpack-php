89bcc290a4451f2892343d206f818a6d3c385d46
Susan Hern <freeublue@gmail.com>
2018-08-02 09:58:52 +0200

    Add files via upload

--- exer/searchresult.php
+++ exer/searchresult.php
@@ -13,7 +13,7 @@ $stm = "SELECT * FROM users WHERE surname LIKE '%$searchTerm%' LIMIT $nu, 10";
 $stmx = "SELECT COUNT(*) FROM users WHERE surname LIKE '%$searchTerm%' "; } 
 elseif($type === 'BirthDate') { 
 $stm = "SELECT * FROM users WHERE birthdate LIKE '%$searchTerm%' LIMIT $nu, 10";
-$stmx = "SELECT COUNT(*) FROM users WHERE birthdatw LIKE '%$searchTerm%'"; }
+$stmx = "SELECT COUNT(*) FROM users WHERE birthdate LIKE '%$searchTerm%'"; }
 elseif($type === 'Mobile') { 
 $stm = "SELECT * FROM users WHERE mobile LIKE '%$searchTerm%' LIMIT $nu, 10 ";
  $stmx = "SELECT COUNT(*) FROM users WHERE mobile LIKE '%$searchTerm%'";}
